003c5da35a204c7031c49333097a65a7733a39f6
vivek <vivekrepale@gmail.com>
2020-07-12 22:31:18 +0530

    Google map emded successfully in production too

--- app/layout/Footer2.js
+++ app/layout/Footer2.js
@@ -9,7 +9,7 @@ const Footer = () => {
 					<div className="footer__offices__office__googleMap">
 						<iframe
 							className="footer__offices__office__googleMap__map"
-							src="http://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.7998357388406!2d73.01596951442907!3d19.072536287089903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c16a8a799c07%3A0x4aea4241a7557ac4!2sSiddharth%20Enterprises!5e0!3m2!1sen!2sin!4v1594566494689!5m2!1sen!2sin"
+							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.7998357388406!2d73.01596951442907!3d19.072536287089903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c16a8a799c07%3A0x4aea4241a7557ac4!2sSiddharth%20Enterprises!5e0!3m2!1sen!2sin!4v1594566494689!5m2!1sen!2sin"
 							// width="600"
 							// height="450"
 							frameBorder="0"
