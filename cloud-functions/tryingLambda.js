c6ac79fe3e47754ec835e09c7f02cafae974bfb1
vivek <vivekrepale@gmail.com>
2020-07-15 22:34:29 +0530

    Extended lamda function 2

--- cloud-functions/tryingLambda.js
+++ cloud-functions/tryingLambda.js
@@ -1,13 +1,13 @@
 exports.handler = function (event, context, callback) {
-	callback(null, {
-		let body;	
-		if(event.body){
-			body = JSON.parse(event.body);
-		}else{
-			body = {}
-		}
+	let body;
+	if (event.body) {
+		body = JSON.parse(event.body);
+	} else {
+		body = {};
+	}
 
+	callback(null, {
 		statusCode: 200,
-		body: body
+		body: body,
 	});
 };
