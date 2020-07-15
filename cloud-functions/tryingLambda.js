355d5023cb884d699bdf0f7c818a698054aae849
vivek <vivekrepale@gmail.com>
2020-07-15 22:30:27 +0530

    Extended lamda function

--- cloud-functions/tryingLambda.js
+++ cloud-functions/tryingLambda.js
@@ -1,6 +1,13 @@
 exports.handler = function (event, context, callback) {
 	callback(null, {
+		let body;	
+		if(event.body){
+			body = JSON.parse(event.body);
+		}else{
+			body = {}
+		}
+
 		statusCode: 200,
-		body: "Reply from netlify lambda function!",
+		body: body
 	});
 };
