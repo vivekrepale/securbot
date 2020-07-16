e6f37a2d51f564016f87b37f604d0dfddd63aa5a
vivek <vivekrepale@gmail.com>
2020-07-16 15:19:50 +0530

    sending mail with gmail

--- cloud-functions/tryingLambda.js
+++ cloud-functions/tryingLambda.js
@@ -1,25 +1,51 @@
 const nodemailer = require("nodemailer");
-const sgTransport = require("nodemailer-sendgrid-transport");
 
-const options = {
-	auth: {
-		api_user: "siddhartenterprises1234@gmail.com",
-		api_key: "Siddharth@9897",
-	},
-};
+// const options = {
+// 	auth: {
+// 		api_user: "siddhartenterprises1234@gmail.com",
+// 		api_key: "Siddharth@9897",
+// 	},
+// };
 
-const client = nodemailer.createTransport(sgTransport(options));
+// const client = nodemailer.createTransport(sgTransport(options));
 
-const email = {
+// const email = {
+// 	from: "siddhartenterprises1234@gmail.com",
+// 	to: "vivekrepale@gmail.com",
+// 	subject: "hello there",
+// 	text: "I am being sent from sendgrid",
+// };
+
+// exports.handler = function (event, context, callback) {
+// 	client
+// 		.sendMail(email)
+// 		.then(() => callback(null, { statusCode: 200, message: "Email sent" }))
+// 		.catch((err) => callback(err, null));
+// };
+
+const sendEmail = aysnc (options) => {
+
+	const transporter = nodemailer.createTransport({
+		service: "Gmail",
+		auth:{
+			user: "siddhartenterprises1234@gmail.com",
+			pass: "Siddharth@9897"
+		}
+	})
+
+	const mailOptions = {
 	from: "siddhartenterprises1234@gmail.com",
 	to: "vivekrepale@gmail.com",
-	subject: "hello there",
-	text: "I am being sent from sendgrid",
+	subject: "Form data",
+	text: "Testing"
+	}
+
+
+	await transporter.sendMail(mailOptions);
 };
 
+
 exports.handler = function (event, context, callback) {
-	client
-		.sendMail(email)
-		.then(() => callback(null, { statusCode: 200, message: "Email sent" }))
-		.catch((err) => callback(err, null));
+	await sendMail().then(() => callback(null, { statusCode: 200, message: "Email sent" }))
+	.catch((err) => callback(err, null))
 };
