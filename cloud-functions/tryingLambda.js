b89eb3c5b414cdf782c98a537ce454a12f93d86d
vivek <vivekrepale@gmail.com>
2020-07-16 14:19:16 +0530

    lambda function send email 1

--- cloud-functions/tryingLambda.js
+++ cloud-functions/tryingLambda.js
@@ -1,8 +1,25 @@
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
+
+const options = {
+	auth: {
+		api_user: "siddharthenterprises1234@gmail.com",
+		api_key: "Siddharth@9897",
+	},
+};
+
+const client = nodemailer.createTransport(sgTransport(options));
+
+const email = {
+	from: "siddharthenterprises1234@gmail.com",
+	to: "vivekrepale@gmail,.com",
+	subject: "hello there",
+	text: "I am being sent from sendgrid",
+};
+
 exports.handler = function (event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({
-			msg: JSON.parse(event.body),
-		}),
-	});
+	client
+		.sendMail(email)
+		.then(() => callback(null, { statusCode: 200, message: "Email sent" }))
+		.catch((err) => callback(err, null));
 };
