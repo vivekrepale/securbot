042ff6ec38633291eb493d0c704478edd3aabeb6
vivek <vivekrepale@gmail.com>
2020-07-16 21:45:36 +0530

    I think i have configured form to email

--- app/components/ContactUsForm.js
+++ app/components/ContactUsForm.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Axios from "axios";
 
 const ContactUsForm = (props) => {
-	const handleSubmit = (e) => {
+	const [name, setName] = useState();
+	const [contact, setContact] = useState();
+	const [pincode, setPincode] = useState();
+	const [requirement, setRequirement] = useState();
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 		//alert("You submitted the form!!");
 		try {
-			Axios.post(
+			await Axios.post(
 				"https://securbot.netlify.app/.netlify/functions/sendEmail",
 				{
-					name: "test",
-					contact: "45454454545",
-					pincode: "4454545",
-					requirement: "Hi test",
+					name: name,
+					contact: contact,
+					pincode: pincode,
+					requirement: requirement,
 				}
 			);
 			console.log("Eamil was successfully sent");
@@ -25,24 +30,28 @@ const ContactUsForm = (props) => {
 		<div className="container">
 			<form onSubmit={handleSubmit} className="container__form">
 				<input
+					onChange={(e) => setName(e.target.value)}
 					type="text"
 					placeholder="Your name!"
 					className="container__form__input"
 				/>
 
 				<input
+					onChange={(e) => setContact(e.target.value)}
 					type="text"
-					placeholder="Your email!"
+					placeholder="Your contact number!"
 					className="container__form__input"
 				/>
 
 				<input
+					onChange={(e) => setPincode(e.target.value)}
 					type="text"
-					placeholder="Your phone no.!"
+					placeholder="Your pincode!"
 					className="container__form__input"
 				/>
 
 				<input
+					onChange={(e) => setRequirement(e.target.value)}
 					type="text"
 					placeholder="Your requirement in short!"
 					className="container__form__input"
