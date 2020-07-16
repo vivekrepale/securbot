d358fe7593e8a5034123338bae0e3106da25fbf3
vivek <vivekrepale@gmail.com>
2020-07-16 13:44:26 +0530

    lambda function

--- cloud-functions/tryingLambda.js
+++ cloud-functions/tryingLambda.js
@@ -1,18 +1,8 @@
 exports.handler = function (event, context, callback) {
-	let body;
-	if (event.body) {
-		body = JSON.parse(event.body);
-	} else {
-		body = {};
-	}
-	const name = event.body.name;
-	const email = event.body.email;
+	const body = JSON.parse(event.body);
 
 	callback(null, {
 		statusCode: 200,
-		body: {
-			name: name,
-			email: email,
-		},
+		body: body,
 	});
 };
