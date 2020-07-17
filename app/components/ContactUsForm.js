a7f1fc3b64dc40848b0f8e10ac0dbe183fe18197
vivek <vivekrepale@gmail.com>
2020-07-17 18:57:37 +0530

    Fixed the contact us button alignment, form is ready

--- app/components/ContactUsForm.js
+++ app/components/ContactUsForm.js
@@ -47,34 +47,41 @@ const ContactUsForm = (props) => {
 					type="text"
 					placeholder="Your name!"
 					className="container__form__input"
+					required
 				/>
 
 				<input
 					onChange={(e) => setContact(e.target.value)}
-					type="text"
-					placeholder="Your contact number!"
+					type="tel"
+					placeholder="Your 10-digit contact number!"
 					className="container__form__input"
+					pattern="[0-9]{10}"
+					required
 				/>
 
 				<input
 					onChange={(e) => setEmail(e.target.value)}
-					type="text"
+					type="email"
 					placeholder="Your contact email!"
 					className="container__form__input"
+					required
 				/>
 
 				<input
 					onChange={(e) => setPincode(e.target.value)}
-					type="text"
-					placeholder="Your pincode!"
+					type="tel"
+					placeholder="Your 6-digit pincode!"
 					className="container__form__input"
+					pattern="[0-9]{6}"
+					required
 				/>
 
 				<input
 					onChange={(e) => setRequirement(e.target.value)}
-					type="text"
+					type="text-box"
 					placeholder="Your requirement in short!"
-					className="container__form__input"
+					className="container__form__input container__form__input--requirement"
+					required
 				/>
 
 				<input
